e4082e07790c0fba40580b9959a247bdc39f160b
Hanna.Lytvynenko <ani-k-a@ukr.net>
2023-07-02 09:52:06 +0200

    add save theme to LS

--- src/js/currentPage/current-page.js
+++ src/js/currentPage/current-page.js
@@ -1,13 +1,17 @@
-
-console.log("hi");
 const setCurrentPage = () => {
     const links = document.querySelectorAll('.header-nav__link');
     const pageUrl = document.URL;
+    const isCurrentPage = false;
 
     links.forEach(el => {
         const currentPage = pageUrl.includes(el.pathname);
-        currentPage && el.classList.add('header-nav__link--active');
+        currentPage && el.classList.add('header-nav__link--active') && (isCurrentPage = true);
+
     })
+    if (!isCurrentPage) {
+        const activePage = document.querySelector('.header-nav__link-main');
+        activePage.classList.add('header-nav__link--active');
+    }
 }
 
 setCurrentPage();
--- src/js/theme/theme.js
+++ src/js/theme/theme.js
@@ -4,14 +4,35 @@ const headerInput = document.querySelector('.header-form__input');
 const headeerFormBtn = document.querySelector('.header-form__btn');
 const themeThumbler = document.querySelector('.theme__thumbler');
 const lable = document.querySelector('.theme__lable');
+const lights = document.querySelectorAll('.light');
+const darks = document.querySelectorAll('.dark');
+
+console.log(localStorage.getItem('theme'));
+
+
+const state = {
+    theme: localStorage.getItem('theme') ? localStorage.getItem('theme') : localStorage.setItem('theme', "light"),
+}
+
 
 
 const toggleTheme = () => {
+    console.log(state);
     body.classList.toggle('dark-theme');
     headerInput.classList.toggle('dark-theme__input');
     headeerFormBtn.classList.toggle('dark-theme__form-btn');
     themeThumbler.classList.toggle('dark-theme__thumbler');
     lable.classList.toggle('dark-theme__lable');
+    lights.forEach(el => el.classList.toggle('accent-icons'));
+    darks.forEach(el => el.classList.toggle('accent-icons'));
 }
 
-btn.addEventListener('change', toggleTheme);
+
+const changeTheme = () => {
+    toggleTheme();
+    localStorage.getItem('theme') === "light" ? localStorage.setItem('theme', 'dark') : localStorage.setItem('theme', 'light')
+};
+
+state.theme === "dark" && toggleTheme();
+
+btn.addEventListener('change', changeTheme);
